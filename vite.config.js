e5b70e985c751c67a96583a936af0dfb1622814b
christian1234t <christianubac789@gmail.com>
2025-10-07 22:41:07 +0800

    fix minor injuries

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "dist",
+  },
   server: {
-    port: 3000
-    
+    port: 5173,
   }
-})
+});
